1dc02cf811b19cb762f2b74d93a369d2a96628a2
deboracaires <debora.caires.mor@gmail.com>
2023-12-19 23:02:36 -0300

    add: servidor nao funcionando

--- app/app.py
+++ app/app.py
@@ -1,11 +1,28 @@
-import asyncio
-import websockets
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-async def chat_server(websocket, path):
-    async for message in websocket:
-        await websocket.send(f"Received: {message}")
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")  # Permitindo todas as origens para fins de teste
 
-if __name__ == "__main__":
-    start_server = websockets.serve(chat_server, "localhost", 8765)
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+# Conjunto para armazenar clientes conectados
+clientes = set()
+
+@socketio.on('message')
+def handle_message(data):
+    nome_usuario = data['nome_usuario']
+    mensagem = data['mensagem']
+
+    # Informa a todos os clientes sobre a nova mensagem
+    socketio.emit('message', {'nome_usuario': nome_usuario, 'mensagem': mensagem}, broadcast=True)
+
+@socketio.on('connect')
+def handle_connect():
+    print('Novo cliente conectado')
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Cliente desconectado')
+
+if __name__ == '__main__':
+    socketio.run(app, host='www.trabalhofinalfrc.com', port=5000)
